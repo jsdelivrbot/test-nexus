3b04debe43bbba81c4b6f1459df59f94723bf292
keletskiy <eletzk@yandex.ru>
2017-06-20 12:52:31 +0400

    Description updated
    small cahnges in .ejs

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ var express = require("express");
 
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
@@ -20,14 +18,16 @@ app.get('/corto', function(req, res) {
     res.render('corto', {});
 });
 
-app.get('/models/:model_id', function(req, res) {
+app.get('/page/:model_id', function(req, res) {
     var arr = fs.readdirSync("public/models/" + req.params.model_id);
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify(arr));
 });
 
 // Start server 
+var port = (process.env.PORT || 5000);
+app.set('port', port);
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  console.log('Server is running - 127.0.0.1:' + port + '/nexus');
 });
