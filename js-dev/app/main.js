f8b571594918e7e6d425eba2acc0f32ab1f7122a
keletskiy <eletzk@yandex.ru>
2017-06-26 12:11:34 +0400

    added worker pool

--- js-dev/app/main.js
+++ js-dev/app/main.js
@@ -2,6 +2,197 @@
 //
 //
 
+var ZipLoaderPool = function(){
+
+    "use strict";
+
+    /* global zip */
+
+    var zip_workers_queue = [];
+    var zip_workers_ative = 0;
+    var zip_workers_max = 6;
+
+    this.OBJUploader = function(in_path, in_filename, in_scene){
+
+        var _self = this;
+
+        _self.filename = in_filename;
+        _self.path = in_path;
+        _self.url = in_path + "/data/" + in_filename + "/data.zip";
+
+        _self.load = function(){
+
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', _self.url, true);
+            xhr.responseType = "blob";
+
+            xhr.onreadystatechange = function () {
+
+                if (xhr.readyState==4 && xhr.status==200) {
+
+                    console.log(xhr.response);
+
+                    var blob = xhr.response;
+
+                    zip_workers_queue.push(blob);
+
+                    zip_worker_interval = setInterval( function(){
+
+                        if (zip_workers_queue.length && 
+                            zip_workers_ative < zip_workers_max){
+                            clearInterval(zip_worker_interval);
+                            zip_worker_interval = 0;
+                            createZipWorker(zip_workers_queue.shift());
+                        }
+
+                    }, 15);
+                    
+                    xhr = null;
+                }
+            };
+            xhr.send();
+        };
+
+        // -----------------------------------------------------------
+
+        var zip_reader, zip_reader_count = 0, zip_worker_interval = 0;
+        var obj_file, mtl_file;
+
+        function readZippedMtlData(text){
+            zip_reader_count--;
+            mtl_file = text;
+            createObject();
+        }
+
+        function readZippedObjData(text){
+            zip_reader_count--;
+            obj_file = text;
+            createObject();
+        }
+
+        function createObject(){
+
+            if (zip_reader_count === 0){ // all files unzipped
+
+                if (zip_reader){
+                    zip_reader.close(function() { }); // onclose callback
+                    zip_workers_ative--;
+                    zip_reader = null;
+                }
+
+                if (obj_file && mtl_file){
+
+                    var mtlLoader = new THREE.MTLLoader();
+                    mtlLoader.setTexturePath( _self.path + "/resources/");
+                    var materials = mtlLoader.parse( mtl_file );
+                    change_image_ref_to_png(materials.materialsInfo);
+                    materials.preload();
+
+                    var objLoader = new THREE.OBJLoader();
+                    objLoader.setMaterials( materials );
+                    objLoader.setPath( _self.path );
+
+                    var object = objLoader.parse( obj_file );
+                    object.position.set(0.0, 0.0, 0.0);
+                    object.scale.set(1.0, 1.0, 1.0);
+                    in_scene.add( object );
+
+                    mtlLoader = null;
+                    objLoader = null;
+                }
+
+                mtl_file = null;
+                obj_file = null;
+            }
+        }
+
+        function createZipWorker(blob){
+
+            zip_workers_ative++;
+
+            zip.createReader(
+                new zip.BlobReader(blob),
+
+                function(reader) {
+
+                    zip_reader = reader;
+
+                    reader.getEntries( function(entries) {
+
+                        if (entries.length) {
+
+                            for (var i = 0; i < entries.length; i++){
+
+                                var filename = entries[i].filename;
+
+                                if (/\.mtl$/.test(filename)){
+                                    zip_reader_count++;
+                                    entries[i].getData(
+                                        new zip.TextWriter(),
+                                        readZippedMtlData,
+                                        null // onprogress callback
+                                    );
+                                }
+                                else if (/\.obj$/.test(filename)){
+                                    zip_reader_count++;
+                                    entries[i].getData(
+                                        new zip.TextWriter(),
+                                        readZippedObjData,
+                                        null // onprogress callback
+                                    );
+                                }
+                            }
+
+                            if (zip_reader_count < 2){
+                                reader.close(function() { }); // onclose callback
+                                zip_reader = null;
+                                zip_workers_ative--;
+                            }
+
+                            if (zip_reader_count > 2)
+                                console.error("zip_reader_count > 2");
+                        }
+                    });
+                },
+                function(error) {}  // onerror callback
+            );
+        }
+
+        function change_image_ref_to_png(mat_info_collection){
+            var bDisableBumpMapping = true;
+            for ( var mat_info_prop in mat_info_collection ) {
+                if (!mat_info_collection.hasOwnProperty(mat_info_prop))
+                    continue;
+                var mat_info = mat_info_collection[mat_info_prop];
+                for ( var prop in mat_info ) {
+                    if (!mat_info.hasOwnProperty(prop))
+                        continue;
+                    var value = mat_info[ prop ];
+                    if ( value === '' ) 
+                        continue;
+                    switch ( prop.toLowerCase() ) {
+                        case 'map_kd':
+                        case 'map_ks':
+                            mat_info[ prop ] = value.replace(/\.[^/.]+$/, "") + ".png";
+                            break;
+                        case 'map_bump':
+                        case 'bump':
+                            if (bDisableBumpMapping){
+                                delete mat_info[prop];
+                            }
+                            else
+                                mat_info[ prop ] = value.replace(/\.[^/.]+$/, "") + ".png";
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+    };
+};
+var ZipLoaderPool = new ZipLoaderPool();
+
 (function main(){
     "use strict";
 
@@ -161,159 +352,6 @@
 
     THREE.Loader.Handlers.add( /\.dds$/i, new THREE.DDSLoader() );
 
-    var change_image_ref_to_png = function(mat_info_collection){
-
-        var bDisableBumpMapping = true;
-
-        for ( var mat_info_prop in mat_info_collection ) {
-
-            if (!mat_info_collection.hasOwnProperty(mat_info_prop))
-                continue;
-
-            var mat_info = mat_info_collection[mat_info_prop];
-
-            for ( var prop in mat_info ) {
-
-                if (!mat_info.hasOwnProperty(prop))
-                    continue;
-
-                var value = mat_info[ prop ];
-
-                if ( value === '' ) 
-                    continue;
-
-                switch ( prop.toLowerCase() ) {
-
-                    case 'map_kd':
-                    case 'map_ks':
-                        mat_info[ prop ] = value.replace(/\.[^/.]+$/, "") + ".png";
-                        break;
-                    case 'map_bump':
-                    case 'bump':
-                        if (bDisableBumpMapping){
-                            delete mat_info[prop];
-                        }
-                        else
-                            mat_info[ prop ] = value.replace(/\.[^/.]+$/, "") + ".png";
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
-    };
-
-
-    function uploadObj(path, filename) {
-
-        var url = path + "/data/" + filename + "/data.zip";
-
-        var xhr = new XMLHttpRequest();
-
-        //xhr.onprogress = calculateAndUpdateProgress;
-        
-        xhr.open('GET', url, true);
-        xhr.responseType = "blob";
-
-        var zip_reader, zip_reader_count = 0, obj_file, mtl_file;
-
-        function readZippedMtlData(text){
-            zip_reader_count--;
-            mtl_file = text;
-            createObject();
-        }
-
-        function readZippedObjData(text){
-            zip_reader_count--;
-            obj_file = text;
-            createObject();
-        }
-
-        function createObject(){
-
-            if (zip_reader_count === 0){ // all files unzipped
-
-                if (zip_reader)
-                    zip_reader.close(function() { }); // onclose callback
-
-                if (obj_file && mtl_file){
-
-                    var mtlLoader = new THREE.MTLLoader();
-                    mtlLoader.setTexturePath( path + "/resources/");
-                    var materials = mtlLoader.parse( mtl_file );
-                    change_image_ref_to_png(materials.materialsInfo);
-                    materials.preload();
-
-                    var objLoader = new THREE.OBJLoader();
-                    objLoader.setMaterials( materials );
-                    objLoader.setPath( path );
-
-                    var object = objLoader.parse( obj_file );
-                    object.position.set(0.0, 0.0, 0.0);
-                    object.scale.set(1.0, 1.0, 1.0);
-                    scene.add( object );
-
-                    mtlLoader = null;
-                    objLoader = null;
-                }
-
-                mtl_file = null;
-                obj_file = null;
-            }
-        }
-
-        xhr.onreadystatechange = function () {
-
-            if (xhr.readyState==4 && xhr.status==200) {
-
-                console.log(xhr.response);
-
-                var blob = xhr.response;
-                
-                // use a BlobReader to read the zip from a Blob object
-                zip.createReader(
-
-                    new zip.BlobReader(blob),
-
-                    function(reader) {
-
-                        zip_reader = reader;
-
-                        reader.getEntries( function(entries) {
-
-                            if (entries.length) {
-
-                                zip_reader_count = entries.length;
-
-                                for (var i = 0; i < entries.length; i++){
-
-                                    var filename = entries[i].filename;
-
-                                    if (/\.mtl$/.test(filename)){
-                                        entries[i].getData(
-                                            new zip.TextWriter(),
-                                            readZippedMtlData,
-                                            null // onprogress callback
-                                        );
-                                    }
-                                    else if (/\.obj$/.test(filename)){
-                                        entries[i].getData(
-                                            new zip.TextWriter(),
-                                            readZippedObjData,
-                                            null // onprogress callback
-                                        );
-                                    }
-                                }
-                            }
-                        });
-                    },
-                    function(error) {}  // onerror callback
-                );
-            }
-        };
-        xhr.send();
-    }
-
     function uploadNxs(path, filename){
         var nexus_obj = new NexusObject(path + filename + ".nxs", renderer, update_nexus_frame);
         nexus_obj.position.set(0.0, 0.0, 0.0);
@@ -383,7 +421,9 @@
                     }
                     else if (upload_format === "obj"){
 
-                        uploadObj( path, models[i]);
+                        var uploader = new ZipLoaderPool.OBJUploader( path, models[i], scene);
+                        uploader.load();
+                        uploader = null;
                     }
                     //else if (upload_format === "crt"){
                     //   if (models[i] === "Digestive.Digestive_exterior.Jejunum")
